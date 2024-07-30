459e64dcb4e4d31371a613c4707104c8abd9b4e3
Anh Ng <anhnk.developer@gmail.com>
2024-07-30 18:12:47 +0700

    Update task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,12 @@ class Task < ApplicationRecord
   belongs_to :user
 
   validates :title, :priority, :due_date, presence: true
+  validates :priority, inclusion: { in: Task.priorities }
+  validates :due_date, comparison: { greater_than: Time.zone.now, message: "can not be in the past" }, on: :create
+
+  scope :incomplete, -> { where(completed: [nil, false]) }
+
+  def completed!
+    update(completed: true)
+  end
 end
