a431b7890270d6c03e6b00587a3f093fe61ed13c
Anh Ng <anhnk.developer@gmail.com>
2024-07-30 18:04:58 +0700

    Add task creation logic

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,24 @@
 class TasksController < ApplicationController
+  layout "task"
+
   def index
     @tasks = fetch_incomplete_tasks
   end
 
   def new
     @task = Task.new(user: current_user)
+  end
+
+  def create
+    @task = Task.create(task_params)
+
+    if @task.errors.blank?
+      flash[:notice] = "Task created successfully"
+      redirect_to :tasks
+    else
+      flash.now[:alert] = "Error: #{@task.errors.full_messages.join(". ")}"
+      render :new, status: 422
+    end
   end
 
   def mark_completed
@@ -24,5 +38,9 @@ class TasksController < ApplicationController
 
   def fetch_incomplete_tasks
     current_user.tasks.incomplete
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :sub_title, :priority, :due_date, :user_id)
   end
 end
