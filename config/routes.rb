ecfb806969ed9393d558a0a1663be8964dfd0965
Anh Ng <anhnk.developer@gmail.com>
2024-07-30 17:56:38 +0700

    Config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "tasks#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   devise_for :users
+
+  resources :tasks, only: %i(index new create)
 end
